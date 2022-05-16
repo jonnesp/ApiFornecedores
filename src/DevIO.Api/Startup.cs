f3f38c37389c9eaf29096b990e1e2b51d115961d
João Moreira <joaobraghin.moreira@gmail.com>
2022-05-16 01:12:48 -0300

    Implementação do JWT de forma simples.

--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -3,17 +3,10 @@ using DevIO.Api.Configuration;
 using DevIO.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevIO.Api
 {
@@ -33,25 +26,12 @@ namespace DevIO.Api
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddIdentityConfiguration(Configuration);
+
             services.AddAutoMapper(typeof(Startup));
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.Configure<ApiBehaviorOptions>(options =>
-            {
-                options.SuppressModelStateInvalidFilter = true;
-            });
-
-            services.AddCors(options =>
-            {
-                options.AddPolicy("Development", builder => builder.AllowAnyOrigin()
-                 .AllowAnyMethod()
-                 .AllowAnyHeader()
-                 .AllowCredentials());
-            });
-
-
+          
+            services.WebApiConfig();
             services.ResolveDependencies();
         }
 
@@ -68,10 +48,8 @@ namespace DevIO.Api
                 app.UseHsts();
             }
 
-            app.UseCors("Development");
+            app.UseMvcConfiguration();
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
         }
     }
 }
