69ef8a57b082710cfe2c16f3954719ee7bc75d6d
João Moreira <joaobraghin.moreira@gmail.com>
2022-05-17 14:35:05 -0300

    Implementação da response vim model.

--- src/DevIO.Api/Controllers/AuthController.cs
+++ src/DevIO.Api/Controllers/AuthController.cs
@@ -82,7 +82,7 @@ namespace DevIO.Api.Controllers
             return CustomResponse(loginUser);
         }
 
-        private async Task<string> GerarJWT(string email)
+        private async Task<LoginResponseViewModel> GerarJWT(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
@@ -119,7 +119,20 @@ namespace DevIO.Api.Controllers
 
             var encodedToken = tokenHandler.WriteToken(token);
 
-            return encodedToken;
+            var response = new LoginResponseViewModel
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                User = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c=> new ClaimViewModel { Type = c.Type, Value = c.Value})
+                }
+            };
+
+
+            return response;
         }
 
         private static long ToUnixEpochDate(DateTime date)
